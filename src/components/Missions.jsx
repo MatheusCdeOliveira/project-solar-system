543f09dcdbc57623d358e3366f0e309f219be89f
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-08-18 18:17:16 -0300

    requisito 9 e 10 feitos

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((e) => (
+          <MissionCard
+            key={ e.year }
+            name={ e.name }
+            year={ e.year }
+            country={ e.country }
+            destination={ e.destination }
+          />
+        ))}
       </div>
     );
   }
